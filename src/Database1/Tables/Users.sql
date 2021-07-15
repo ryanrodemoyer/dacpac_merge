2fb1ff9775b98faa36b93b13e3a49109790426cf
Ryan Rodemoyer <ryan.rodemoyer@mortgagecadence.com>
2021-07-15 13:43:08 -0600

    add Users table to both dacpacs to confirm that the merge strategy is Last In Wins

---
+++ src/Database1/Tables/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(100) NULL, 
+    [DateJoined] DATETIME NULL
+)
---
+++ src/Database2/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(100) NULL, 
+    [DateJoined] DATETIME NOT NULL, 
+    [AccountExpires] DATETIME NULL
+)
